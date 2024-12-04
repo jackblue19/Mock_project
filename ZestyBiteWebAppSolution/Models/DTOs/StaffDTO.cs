4b634ca29921b43f0f4017eaabd37207446d3a9e
Jack Blue <git.jack1624@gmail.com>
2024-12-04 15:49:02 +0700

    done merge main 04-12-2024

---
+++ ZestyBiteWebAppSolution/Models/DTOs/StaffDTO.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZestyBiteWebAppSolution.Models.DTOs
+{
+    public class StaffDTO
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(255, ErrorMessage = "Username cannot exceed 255 characters.")]
+        public string Username { get; set; } = null!;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long and not exceed 100 characters.")]
+        public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Phone Number is required.")]
+        [Phone(ErrorMessage = "Invalid phone number.")]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required(ErrorMessage = "Address is required.")]
+        public string? Address { get; set; }
+
+        [Required(ErrorMessage = "Gender is required.")]
+        public int Gender { get; set; }
+
+        public string ProfileImg { get; set; } = "default null";
+
+        public string RoleDescription { get; set; } = null!;
+    }
+}

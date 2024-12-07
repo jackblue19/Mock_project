7b85398c6243e3c0cba78f96d26cdabac7a35b80
khanhtqde160630 <khanh0976109385@gmail.com>
2024-12-08 03:42:24 +0700

    fe done

---
+++ ZestyBiteWebAppSolution/Areas/Manager/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZestyBiteWebAppSolution.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MenuManager()
+        {
+            return View();
+        }
+        public IActionResult payHistory()
+        {
+            return View();
+        }
+        public IActionResult AccountManagement1()
+        {
+            return View();
+        }
+
+
+    }
+}
